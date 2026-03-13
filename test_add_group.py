a76fbf370c14007028e6efe48ba2df8309f59e4e
Artem Latyshev <iamsorealaptem@gmail.com>
2026-03-13 18:52:51 +0700

    урок №2.2. фикстура

--- test_add_group.py
+++ test_add_group.py
@@ -1,57 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from group import Group
+from application import Application
+import pytest
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy())
+    return fixture
+
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group_name1", header="group header", footer="group footer"))
+    app.return_to_groups_page()
+    app.logout()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
 
-class TestTestaddgroup():
-    def setup_method(self, method):
-        self.wd = webdriver.Firefox()
-        self.vars = {}
 
-    def teardown_method(self, method):
-        self.wd.quit()
-
-    def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="group_name1", header="group header", footer="group footer"))
-        self.return_to_groups_page()
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.create_group(Group(name="", header="", footer=""))
-        self.logout()
-
-    def logout(self):
-        self.wd.find_element(By.LINK_TEXT, "Logout").click()
-        self.wd.find_element(By.CSS_SELECTOR, "html").click()
-
-    def return_to_groups_page(self):
-        self.wd.find_element(By.LINK_TEXT, "group page").click()
-
-    def create_group(self, group):
-        self.open_groups_page()
-        # init group creation
-        self.wd.find_element(By.NAME, "new").click()
-        # fill group form
-        self.wd.find_element(By.NAME, "group_name").click()
-        self.wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        self.wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        self.wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        # submit group creation
-        self.wd.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page()
-
-    def open_groups_page(self):
-        self.wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, username, password):
-        self.open_home_page()
-        self.wd.find_element(By.NAME, "user").click()
-        self.wd.find_element(By.NAME, "user").send_keys(username)
-        self.wd.find_element(By.NAME, "pass").send_keys(password)
-        self.wd.find_element(By.CSS_SELECTOR, "input:nth-child(7)").click()
-
-    def open_home_page(self):
-        self.wd.get("https://localhost/addressbook/")
-        self.wd.set_window_size(1006, 892)
