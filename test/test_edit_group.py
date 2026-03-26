84b35ea788f44fd61119d4e5211198cb010d77bf
Artem Latyshev <iamsorealaptem@gmail.com>
2026-03-26 14:10:31 +0700

    Урок "Оптимизация ввода данных в формыСтраница".

--- test/test_edit_group.py
+++
@@ -1,8 +0,0 @@
-from model.group import Group
-
-def test_edit_group(app):
-    app.open_home_page()
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="changed group name1", header="changed group header", footer="changed group footer"))
-    app.group.return_to_groups_page()
-    app.session.logout()
